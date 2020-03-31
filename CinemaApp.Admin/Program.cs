a026e2aa0123a1637343a322e5c7127a2d344417
Andyong8901 <59492418+Andyong8901@users.noreply.github.com>
2020-04-01 06:28:35 +0800

    upload

---
+++ CinemaApp.Admin/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Create Customer Data");
+            Console.WriteLine("1.1 Delete Customer Data");
+            Console.WriteLine("2. Create All Movie Data");
+            Console.WriteLine("2.1 Delete All Movie Data");
+            Console.WriteLine("3. Create Hall Data");
+            Console.WriteLine("3.1 Delete Hall Data");
+            Console.WriteLine("4. Create Movie Hall Data");
+            Console.WriteLine("4.1 Delete Movie Hall Data");
+            Console.WriteLine("5. Create Movie Hall Seat");
+            Console.WriteLine("5.1 Delete Movie Hall Seat");
+            double Option = Convert.ToDouble(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    break;
+                case 1.1:
+                    break;
+                case 2:
+                    break;
+                case 2.1:
+                    break;
+                case 3:
+                    break;
+                case 3.1:
+                    break;
+                case 4:
+                    break;
+                case 4.1:
+                    break;
+                case 5:
+                    break;
+                case 5.1:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
